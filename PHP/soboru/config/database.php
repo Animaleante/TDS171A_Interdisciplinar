51962bc661a76ee1c74fe055e2aec1043342803e
Diogo S. Moraes <srto.dih@gmail.com>
2017-09-25 16:56:36 -0300

    Oracle finally

--- PHP/soboru/config/database.php
+++ PHP/soboru/config/database.php
@@ -78,6 +78,21 @@ return [
             'prefix' => '',
         ],
 
+        'oracle' => [
+            'driver' => 'oracle',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            // 'tns' => 'xe',
+            // 'database' => env('DB_DATABASE', 'forge'),
+            'database' => 'xe',
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+        ]
+
     ],
 
     /*
