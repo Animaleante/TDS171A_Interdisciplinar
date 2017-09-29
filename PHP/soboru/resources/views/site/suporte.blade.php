ec3cfa2fd1659ab2f2580c4b5a731488574171a5
Diogo Sota Moraes <diogo@cht.net.br>
2017-09-29 17:45:09 -0300

    View Suporte

---
+++ PHP/soboru/resources/views/site/suporte.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.site.master')
+
+@section('content')
+    <main class="main" role="main">
+        <div class="wrap clearfix">
+            <div class="row">
+                <section class="content center full-width">
+                    <div class="modal container">
+                        <form method="post" action="contact_form_message.php" name="contactform" id="contactform">
+                            <h3>Suporte</h3>
+                            <div id="message" class="alert alert-danger"></div>
+                            <div class="f-row">
+                                <input type="text" placeholder="Your name" id="name" />
+                            </div>
+                            <div class="f-row">
+                                <input type="email" placeholder="Your email" id="email" />
+                            </div>
+                            <div class="f-row">
+                                <input type="number" placeholder="Your phone number" id="phone" />
+                            </div>
+                            <div class="f-row">
+                                <textarea placeholder="Your message" id="comments"></textarea>
+                            </div>
+                            <div class="f-row bwrap">
+                                <input type="submit" value="Send message" />
+                            </div>
+                        </form>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </main>
+@endsection
