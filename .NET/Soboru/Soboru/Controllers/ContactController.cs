8fb14adde6d22e8f15f7611df761850398eb7180
Luiz Palte <phelipepalte@gmail.com>
2017-10-07 22:47:03 -0300

    Incluido controller Contact, view Contact e formatado no padrão das páginas. Erro no nav.

---
+++ .NET/Soboru/Soboru/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Soboru.Controllers
+{
+    public class ContactController : Controller
+    {
+        private string controllerName = "Contact";
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.ControllerName = controllerName;
+            return View();
+        }
+    }
+}
