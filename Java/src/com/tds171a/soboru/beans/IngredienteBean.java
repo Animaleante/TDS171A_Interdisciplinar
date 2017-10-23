3b473c4763ca776aa6d14a6a3329de0aa2b3a1f4
tgietds <tgietds@RB-LAB102B-08.Academico.edu>
2017-10-23 21:32:46 -0200

    Funcionando...kindof

--- Java/src/com/tds171a/soboru/beans/IngredienteBean.java
+++ Java/src/com/tds171a/soboru/beans/IngredienteBean.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.tds171a.soboru.beans;
 
@@ -7,6 +7,8 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 import com.tds171a.soboru.controllers.IngredienteController;
@@ -21,29 +23,51 @@ import com.tds171a.soboru.vos.Ingrediente;
 public class IngredienteBean implements Serializable {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 8410408634179869866L;
-	
+
+	private String routeBase = "/ingrediente/";
+
 	private IngredienteController controller;
 	private Ingrediente ingrediente;
 	private List<Ingrediente> ingredienteLista;
 
 	/**
-	 * 
+	 *
 	 */
 	public IngredienteBean() {
 		controller = new IngredienteController();
 		setIngrediente(new Ingrediente());
 		//setIngredienteLista(controller.listar());
 	}
-	
+
 	public String listar() {
 		setIngredienteLista(controller.listar());
-		
+
 		System.out.println("Lista size: " + getIngredienteLista().size());
-		
-		return "/ingrediente/index";
+
+		return routeBase + "index";
+	}
+
+	public String incluir() {
+	    FacesContext context = FacesContext.getCurrentInstance();
+
+	    if(getIngrediente().getNome().isEmpty()) {
+	        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Nome não pode vazio!", null));
+	        return routeBase + "create";
+	    }
+
+	    if(controller.incluir(getIngrediente())) {
+	        context.addMessage(null,  new FacesMessage(FacesMessage.SEVERITY_INFO, "Ingrediente cadastrado com sucesso!", null));
+	    } else {
+	        context.addMessage(null,  new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ingrediente não foi cadastrado!", null));
+            return routeBase + "create";
+	    }
+
+	    setIngrediente(new Ingrediente());
+
+	    return routeBase + "index";
 	}
 
 	/**
