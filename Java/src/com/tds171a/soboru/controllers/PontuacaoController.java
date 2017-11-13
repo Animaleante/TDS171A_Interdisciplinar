1621b38c8a0b61fb9efea45a5d9e443d6deba567
Luiz Phelipe Palte <phelipepalte@gmail.com>
2017-11-13 16:23:17 -0200

    Corrigido javadocs de controllers

--- Java/src/com/tds171a/soboru/controllers/PontuacaoController.java
+++ Java/src/com/tds171a/soboru/controllers/PontuacaoController.java
@@ -7,14 +7,15 @@ import com.tds171a.soboru.models.pontuacao.PontuacaoModel;
 import com.tds171a.soboru.vos.Pontuacao;
 
 /**
+ * Classe de controller da pontuacao
  * @author Diogo
  *
  */
 public class PontuacaoController extends ControllerBase<Pontuacao> {
 	
 	/**
-	 * 
-	 */
+     * Construtor da classe de controller da pontuacao
+     */
 	public PontuacaoController() {
 		model = new PontuacaoModel();
 	}
--- Java/src/com/tds171a/soboru/controllers/UsuarioController.java
+++ Java/src/com/tds171a/soboru/controllers/UsuarioController.java
@@ -4,16 +4,23 @@ import com.tds171a.soboru.models.usuario.UsuarioModel;
 import com.tds171a.soboru.vos.Usuario;
 
 /**
- * 
+ * Classe de controller do Usuario
  * @author Diogo
  *
  */
 public class UsuarioController extends ControllerBase<Usuario> {
 	
+	/**
+	 *Construtor do usuáro 
+	 */
 	public UsuarioController() {
 		model = new UsuarioModel();
 	}
 
+	/**
+	 * método que retorna um objeto do tipo usuario
+	 * com o usuario logado.
+	 */
 	public Usuario loginUsuario(String email, String senha) {
 		return ((UsuarioModel)model).loginUsuario(email, senha);
 	}
